fc418f620ef81a6d1dcc697856e81760bb6ab0f0
AmenAbiy <abiy865@revature.net>
2022-08-10 23:47:44 -0400

    Arrays Exercise

---
+++ ReverseExcercise/Reverse1.java
@@ -0,0 +1,11 @@
+public class Reverse1 {
+    public static void main(String[] args) {
+        String names[] = {{"Dave"},{"Micheal"}, {"Aron"},{"John"}, {"Alisson"}}
+                names.reverse();
+                for(i=0, i< names.length, ++i){
+                    for(j=0, j<names.length, ++j)
+                }
+        System.out.println(names[i][j]);
+                ++i;
+    }
+}
---
+++ ReverseExcercise/src/Reverse2.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+//import java.util.function.;
+//public class Reverse2 {
+//    public static void main(String[] args) {
+//        String names[] = {"Dave","Micheal", "Aron","John", "Alisson"};
+//    int i = 0;
+//    while(i < 10) {
+//    }
+//       StringBuffer sbr = new StringBuffer(names[i]);
+//        sbr.reverse();
+//        System.out.println(sbr);
+//        i++;
+//    }
+//}
+public class Reverse2 {
+    public static void main(String[] args) {
+        int num[] = {10,5,4,3,6,2,1,,8,9,7};
+        for (i<0; i<num.length; i++){
+            Arrays.stream(num).sorted();
+
+        }
+    }
+}
